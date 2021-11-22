c224acff8446420b2a5632950982055dc540ee38
hik8hik <hik8hik@gmail.com>
2021-11-22 20:26:26 +0000

    Added a flight passenger capacity checker function

--- examples/classes/classes.py
+++ examples/classes/classes.py
@@ -14,9 +14,32 @@ print(p.y)
 
 
 class FLight():
+    # FUNCTION FOR CAPACITY
     def __init__(self, capacity):
         self.capacity = capacity
         self.passengers = []
 
+    # FUNCTION TO APPEND PASSENGERS WITH NAME
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    # FUNCTION TO GET EMPTY SEATS
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
 
 flight = FLight(3)
+print(flight)
+
+people = ["HIK HIK", "HIKAL LAKIH", "LAKIH HIKAL", "OTHER USER"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"Added {person}, ")
+        # PRINT: Added.....
+    else:
+        print(f"No available seats for {person} ")
+        # PRINT: No available seats for OTHER USER
