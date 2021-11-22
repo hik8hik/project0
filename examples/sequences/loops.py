e362c77924b621df59d2d2cd4fc58a1038c6628a
hik8hik <hik8hik@gmail.com>
2021-11-22 11:14:15 +0000

    Moved loops to sequences

---
+++ examples/sequences/loops.py
@@ -0,0 +1,19 @@
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+# RANGE
+for i in range(6):
+    print(i)
+
+
+# FORLOOP IN STRING ARRAY
+names = ["Harry", "Hik", "Linkin", "Park"]
+
+for name in names:
+    print(name)
+
+# FORLOOP IN STRINGS
+name = "LinkinPark"
+
+for character in name:
+    print(character)
