84c0021d90ac20635efe352f7093508227e96624
hik8hik <hik8hik@gmail.com>
2021-11-22 04:48:42 +0000

    Added a line to print one item at pos 0 of the list

--- examples/sequences.py
+++ examples/sequences.py
@@ -1,13 +1,15 @@
 name = "Harry"
-names = ["Harry","Hik","Linkin","Park"]
+names = ["Harry", "Hik", "Linkin", "Park"]
 
 
 # PRINTS OUT THE FIRST LETTER OF HARRY'S NAME
-print(name[0])# H
+print(name[0])  # H
 
 # PRINTS OUT THE FIRST LETTER OF HARRY'S NAME
-print(name[1])# a
+print(name[1])  # a
 
 # PRINTS OUT THE WHOLE LIST
-print(names)# ["Harry","Hik","Linkin","Park"]
+print(names)  # ["Harry","Hik","Linkin","Park"]
 
+# PRINTS OUT THE POSITIONED ITEM
+print(names[0])  # ["Harry","Hik","Linkin","Park"]
