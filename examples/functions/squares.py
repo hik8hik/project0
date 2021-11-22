a10d4ea20df18db254abb59d27c88b705667165b
hik8hik <hik8hik@gmail.com>
2021-11-22 18:16:55 +0000

    Imported squares from functions.py

--- examples/functions/squares.py
+++ examples/functions/squares.py
@@ -1,3 +1,6 @@
+# IMPORTING FUNCTION(square) FROM functions file
+from functions import square
+
 # PRINTING NUMBER AND ITS SQUARE IN RANGE OF 10
 for i in range(10):
     print(f"The square of {i} is {square(i)}")
