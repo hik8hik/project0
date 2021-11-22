7bfc75183ad73f6d4eb4e53db665e58533d497fd
hik8hik <hik8hik@gmail.com>
2021-11-22 04:46:44 +0000

    Added a line to print the whole list

--- examples/sequences.py
+++ examples/sequences.py
@@ -3,7 +3,11 @@ names = ["Harry","Hik","Linkin","Park"]
 
 
 # PRINTS OUT THE FIRST LETTER OF HARRY'S NAME
-print(name[0])#H
+print(name[0])# H
 
 # PRINTS OUT THE FIRST LETTER OF HARRY'S NAME
-print(name[1])#a
+print(name[1])# a
+
+# PRINTS OUT THE WHOLE LIST
+print(names)# ["Harry","Hik","Linkin","Park"]
+
