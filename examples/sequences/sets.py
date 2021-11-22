b937991d0492bc396afc02dc249e81dcb394ace2
hik8hik <hik8hik@gmail.com>
2021-11-22 06:05:39 +0000

    Simple explanation of sets

---
+++ examples/sequences/sets.py
@@ -0,0 +1,7 @@
+# NOTE: SETS - Sets are different from lists and tuples in that they are unordered.
+# They are also different because while you can have two or more of the same elements
+# within a list/tuple, a set will only store each value once. We can define an empty set
+# using the set function. We can then use add and remove to add and remove elements from
+# that set, and the len function to find the set’s size. Note that the len function works on
+# all sequences in python. Also note that despite adding 4 and 3 to the set twice, each item
+# can only appear once in a set.
