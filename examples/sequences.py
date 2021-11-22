2f697b08716452f258a2a73489e01033937acf68
hik8hik <hik8hik@gmail.com>
2021-11-22 05:01:47 +0000

    Added a topple

--- examples/sequences.py
+++ examples/sequences.py
@@ -1,13 +1,20 @@
+#NOTE: STRINGS
 name = "Harry"
+
+#NOTE: LIST
 names = ["Harry", "Hik", "Linkin", "Park"]
 
+#NOTE: TOPPLE
+coordinate = (10.0, 20.0)
 
+#NOTE: STRINGS
 # PRINTS OUT THE FIRST LETTER OF HARRY'S NAME
 print(name[0])  # H
 
 # PRINTS OUT THE FIRST LETTER OF HARRY'S NAME
 print(name[1])  # a
 
+#NOTE: LIST
 # PRINTS OUT THE WHOLE LIST
 print(names)  # ["Harry","Hik","Linkin","Park"]
 
