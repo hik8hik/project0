0f44e9fe99749ee7a3ff882c19bf9f33a6cd763f
hik8hik <hik8hik@gmail.com>
2021-11-22 21:00:45 +0000

    Taught sort function how to sort dicionaries

--- examples/functions/lambda.py
+++ examples/functions/lambda.py
@@ -1,6 +1,17 @@
+# LIST AND EVERY ELEMENT IS A DICTIONARY
 people = [
     {"name": "Hik HIk", "Age": "24"},
     {"name": "Hikal Hiker", "Age": "22"},
     {"name": "Test Tester", "Age": "22"},
     {"name": "Linkin PArk", "Age": "50"}
 ]
+
+#teaching function sort how to sort ☝
+
+def f(person):
+    return person["name"]
+
+#PASSING key as f()
+people.sort(key=f)
+
+print(people)
