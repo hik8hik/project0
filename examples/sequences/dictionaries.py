08e9963fd4628f819328e7531743270a2ce58469
hik8hik <hik8hik@gmail.com>
2021-11-22 17:54:52 +0000

    Changed a value of a dictionary and printed the dict

--- examples/sequences/dictionaries.py
+++ examples/sequences/dictionaries.py
@@ -4,3 +4,10 @@ people = {"Hik Hik": "Temperamental", "Linkin Park": "Social",
 
 # PRINTING THE VALUE OF HIK HIK // (printing the character of Hik Hik)
 print(people["Hik Hik"])  # Temperamental
+
+
+# CHANGING THE VALUE OF A SPECIFIC DICTIONARY
+people["Linkin Park"] = "Emotional"
+
+# PRINTING THE VALUE OF HIK HIK // (printing after value change)
+print(people["Linkin Park"])  # Emotional
