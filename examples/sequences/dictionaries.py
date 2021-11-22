71d8c4aa584692d259226520de8ff328997f3048
hik8hik <hik8hik@gmail.com>
2021-11-22 13:09:29 +0000

    Printed the value of Hik Hik

--- examples/sequences/dictionaries.py
+++ examples/sequences/dictionaries.py
@@ -1,2 +1,6 @@
 people = {"Hik Hik": "Temperamental", "Linkin Park": "Social",
           "Paramore": "Nice", "Dahab": "Caring"}
+
+
+# PRINTING THE VALUE OF HIK HIK // (printing the character of Hik Hik)
+print(people["Hik Hik"])  # Temperamental
