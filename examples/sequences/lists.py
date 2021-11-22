e34fd0ebf378b99d90fbbbe33a24bfa0648a2d97
hik8hik <hik8hik@gmail.com>
2021-11-22 05:58:27 +0000

    Add another item in the list *** sorted the list in alphabetic order *** Printed the list

--- examples/sequences/lists.py
+++ examples/sequences/lists.py
@@ -1,2 +1,11 @@
 #NOTE: LIST - Secuence of mutable values
 names = ["Harry", "Hik", "Linkin", "Park"]
+
+#ADDS ANOTHER ITEM IN THE LIST
+names.append("Paramore")
+
+#SORT THE LIST IN APLHABETIC ORDER
+names.sort()
+
+#PRINTS THE LIST
+print(names)
